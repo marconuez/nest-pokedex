6d6b07977f7860db642af42b49a8edd2de9a923e
marconuez <marco1997.n.c@gmail.com>
2025-01-10 16:28:16 -0600

    Antes de insertart por lote

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -6,6 +6,7 @@ import { PokeResponse } from './interfaces/poke-response.interface';
 export class SeedService {
 
   private readonly axios: AxiosInstance = axios;
+  //ANTES DE INSERTAR POR LOTE
 
   async executeSeed() {
     const {data} = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')
